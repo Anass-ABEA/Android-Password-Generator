d55a7d0d240d73f56157b2c1e7a91cb4f15ee30d
Anass ABEA <annassabe@gmail.com>
2022-08-07 16:32:07 +0100

    password generator v1.0

---
+++ app/src/main/java/com/thexcoders/mypasswordgenerator/models/UpperCaseGenerator.java
@@ -0,0 +1,12 @@
+package com.thexcoders.mypasswordgenerator.models;
+
+public class UpperCaseGenerator extends PasswordGenerator{
+    private static final char CHAR_A = 'A';
+    private static final char CHAR_Z = 'Z';
+
+
+    @Override
+    public String getChar() {
+        return String.valueOf((char) (Helper.randomChar(CHAR_A,CHAR_Z)));
+    }
+}
